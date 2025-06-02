5608e21653614023c28bba9740f2d30bb26b8bfb
unexcellent <>
2025-06-02 14:51:37 +0200

    feat: Part.is_empty()

--- src/parts/part.rs
+++ src/parts/part.rs
@@ -28,6 +28,19 @@ impl Part {
         Self { inner: None }
     }
 
+    /// Return true if this `Part` is empty.
+    ///
+    /// ```rust
+    /// use anvil::{Cube, IntoLength, Part};
+    ///
+    /// let cube = Cube::from_size(1.m());
+    /// assert!(!cube.is_empty());
+    /// assert!(cube.subtract(&cube).is_empty());
+    /// ```
+    pub fn is_empty(&self) -> bool {
+        self.volume() < 1e-9
+    }
+
     /// Merge this `Part` with another.
     ///
     /// ```rust
